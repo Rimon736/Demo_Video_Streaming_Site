fb562b78c81f986dd757bb07082f216539b99115
Rimon736 <haqueiftekharul84@gmail.com>
2025-03-16 02:01:30 +0600

    Videos Section

--- Code/scripts/index.js
+++ Code/scripts/index.js
@@ -2,7 +2,7 @@
 
 const buttonReaction=()=>{
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-    .then(res=>res.json())
+    .then(res => res.json())
     .then(data=> categoryButton(data.categories))
 }
 
@@ -21,4 +21,48 @@ const categoryButton=(categories)=>{
     }
 }
 
+const videofetch=()=>{
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then(data => videoCard(data.videos))
+}
+
+const videoCard = (videos) => {
+    const vidContainer=document.getElementById("videos_container");
+
+    for(let vid of videos){
+
+        const vidDiv= document.createElement("div")
+        vidDiv.innerHTML=`
+        <div class="card bg-base-100 shadow-sm rounded">
+                <figure class="relative">
+                    <img class="rounded h-[150px] object-cover"
+                    src="${vid.thumbnail}"
+                    alt="${vid.thumbnail}" />
+                    <small class="absolute text-[14px] text-white bg-black bottom-2 right-2 p-1 rounded">3days 2 hours</small>
+                </figure>
+                <div class=" flex flex-row p-2">
+                    <div class="m-2"> <img src="${vid.authors[0].profile_picture}" alt="profile picture" class="rounded-[50%] object-cover w-10 h-10 p-0"></div>
+                    <div class="m-2">
+                        <div>
+                            <h1 class="font-bold">${vid.title}</h1>
+                        </div>
+                        <div class="flex flex-row p-0">
+                            <p class="text-slate-500 text-[14px]">${vid.authors[0].profile_name}</p>
+                            <img class="w-4 h-4 mx-2" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt="verified">
+                        </div>
+                        <div>
+                            <p class="text-slate-500">${vid.others.views}</p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+        `
+
+        vidContainer.appendChild(vidDiv);
+    }
+}
+
+
 buttonReaction();
+videofetch();
